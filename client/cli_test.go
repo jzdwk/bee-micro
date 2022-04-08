44055811d48948b27c7502476f8f013a41fd3ad4
formero <425953474@qq.com>
2022-04-08 17:52:57 +0800

    集成客户端jaeger

--- client/cli_test.go
+++ client/cli_test.go
@@ -2,26 +2,41 @@ package client
 
 import (
 	httpClient "bee-micro/client/httpclient"
-	"bee-micro/client/wrappers"
 	"bee-micro/config"
 	"context"
 	"github.com/asim/go-micro/plugins/registry/consul/v3"
+	micro_opentracing "github.com/asim/go-micro/plugins/wrapper/trace/opentracing/v3"
 	"github.com/asim/go-micro/v3/client"
 	"github.com/asim/go-micro/v3/registry"
 	"github.com/asim/go-micro/v3/selector"
+	"github.com/opentracing/opentracing-go"
+	"github.com/uber/jaeger-client-go"
+	jaeger_config "github.com/uber/jaeger-client-go/config"
+	"io"
+	"log"
+
+	//opentracing "github.com/opentracing/opentracing-go"
 	"testing"
 	"time"
 )
 
 func TestHttpCli(t *testing.T) {
+	//tracing
+	tracer, io, err := NewTracer("http-demo-tracing-cli", "192.168.143.146:6831")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer io.Close()
+	opentracing.SetGlobalTracer(tracer)
+
 	//读取配置中心
 	cfg, _ := config.GetConfig()
 
-	info, _ := config.GetConsul(cfg, "consul")
+	info, _ := config.GetConfigInfo(cfg, "config")
 
 	//get service reg
 	reg := consul.NewRegistry(func(options *registry.Options) {
-		options.Addrs = []string{info.Address}
+		options.Addrs = []string{info.Consul.Address}
 	})
 
 	//get service selector
@@ -35,7 +50,9 @@ func TestHttpCli(t *testing.T) {
 		client.DialTimeout(time.Second*10),
 		client.RequestTimeout(time.Second*10),
 		//3. hystrix
-		client.Wrap(wrappers.NewHystrixWrapper()),
+		//client.Wrap(wrappers.NewHystrixWrapper()),
+		//tracing
+		client.Wrap(micro_opentracing.NewClientWrapper(opentracing.GlobalTracer())),
 	)
 
 	doGetRequest(t, c)
@@ -71,4 +88,21 @@ func doPostRequest(t *testing.T, c client.Client) {
 type Resp struct {
 	Method  string
 	Message string
+}
+
+//创建链路追踪实例
+func NewTracer(serviceName string, addr string) (opentracing.Tracer, io.Closer, error) {
+	cfg := &jaeger_config.Configuration{
+		ServiceName: serviceName,
+		Sampler: &jaeger_config.SamplerConfig{
+			Type:  jaeger.SamplerTypeConst,
+			Param: 1,
+		},
+		Reporter: &jaeger_config.ReporterConfig{
+			BufferFlushInterval: 1 * time.Second,
+			LogSpans:            true,
+			LocalAgentHostPort:  addr,
+		},
+	}
+	return cfg.NewTracer()
 }
